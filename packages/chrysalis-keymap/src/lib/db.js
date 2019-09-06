e00af235626c050a6b60652a285525579fe13eb8
anton-voron <anton.voron.gt@gmail.com>
2019-09-06 19:12:58 +0300

    db update language change

--- packages/chrysalis-keymap/src/lib/db.js
+++ packages/chrysalis-keymap/src/lib/db.js
@@ -14,143 +14,141 @@
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import BlankTable from "./db/blanks"
-import LetterTable, { ModifiedLetterTables } from "./db/letters"
-import DigitTable, { ModifiedDigitTables } from "./db/digits"
-import { LockLayerTable, ShiftToLayerTable } from "./db/layerswitch"
-import PunctuationTable, {
-    ModifiedPunctuationTables
-} from "./db/punctuation"
-import SpacingTable, { ModifiedSpacingTables } from "./db/spacing"
+import BlankTable from "./db/blanks";
+import LetterTable, { ModifiedLetterTables } from "./db/letters";
+import DigitTable, { ModifiedDigitTables } from "./db/digits";
+import { LockLayerTable, ShiftToLayerTable } from "./db/layerswitch";
+import PunctuationTable, { ModifiedPunctuationTables } from "./db/punctuation";
+import SpacingTable, { ModifiedSpacingTables } from "./db/spacing";
 import ModifiersTable, {
-    ModifiedModifiersTables,
-    HyperMehTable
-} from "./db/modifiers"
-import NavigationTable, {
-    ModifiedNavigationTables
-} from "./db/navigation"
-import LEDEffectsTable from "./db/ledeffects"
-import MacrosTable from "./db/macros"
-import NumpadTable, {
-    ModifiedNumpadTables
-} from "./db/numpad"
-import FunctionKeyTable, {
-    ModifiedFunctionKeyTables
-} from "./db/fxs"
+  ModifiedModifiersTables,
+  HyperMehTable
+} from "./db/modifiers";
+import NavigationTable, { ModifiedNavigationTables } from "./db/navigation";
+import LEDEffectsTable from "./db/ledeffects";
+import MacrosTable from "./db/macros";
+import NumpadTable, { ModifiedNumpadTables } from "./db/numpad";
+import FunctionKeyTable, { ModifiedFunctionKeyTables } from "./db/fxs";
 
-import MediaControlTable from "./db/mediacontrols"
+import MediaControlTable from "./db/mediacontrols";
 import {
-    MouseMovementTable,
-    MouseWheelTable,
-    MouseButtonTable,
-    MouseWarpTable
-} from "./db/mousecontrols"
-import MiscellaneousTable from "./db/miscellaneous"
+  MouseMovementTable,
+  MouseWheelTable,
+  MouseButtonTable,
+  MouseWarpTable
+} from "./db/mousecontrols";
+import MiscellaneousTable from "./db/miscellaneous";
 
-import { OneShotModifierTable, OneShotLayerTable } from "./db/oneshot"
-import { DualUseModifierTables, DualUseLayerTables } from "./db/dualuse"
-import TapDanceTable from "./db/tapdance"
-import LeaderTable from "./db/leader"
-import StenoTable from "./db/steno"
-import SpaceCadetTable from "./db/spacecadet"
+import { OneShotModifierTable, OneShotLayerTable } from "./db/oneshot";
+import { DualUseModifierTables, DualUseLayerTables } from "./db/dualuse";
+import TapDanceTable from "./db/tapdance";
+import LeaderTable from "./db/leader";
+import StenoTable from "./db/steno";
+import SpaceCadetTable from "./db/spacecadet";
+import spanish from "./languages/spanish/spanish";
+import withLanguageLayout from "./languages/withLanguageLayout";
 
-const baseKeyCodeTable = [
-    LetterTable,
-    DigitTable,
-    PunctuationTable,
-    SpacingTable,
-    ModifiersTable,
-    NavigationTable,
-    FunctionKeyTable,
-    NumpadTable,
-    MiscellaneousTable,
+let baseKeyCodeTable = [
+  LetterTable,
+  DigitTable,
+  PunctuationTable,
+  SpacingTable,
+  ModifiersTable,
+  NavigationTable,
+  FunctionKeyTable,
+  NumpadTable,
+  MiscellaneousTable,
 
-    ShiftToLayerTable,
-    LockLayerTable,
+  ShiftToLayerTable,
+  LockLayerTable,
 
-    LEDEffectsTable,
-    MacrosTable,
-    MediaControlTable,
-    MouseMovementTable,
-    MouseButtonTable,
-    MouseWheelTable,
-    MouseWarpTable,
+  LEDEffectsTable,
+  MacrosTable,
+  MediaControlTable,
+  MouseMovementTable,
+  MouseButtonTable,
+  MouseWheelTable,
+  MouseWarpTable,
 
-    OneShotModifierTable,
-    OneShotLayerTable,
-    TapDanceTable,
-    LeaderTable,
-    StenoTable,
-    SpaceCadetTable,
+  OneShotModifierTable,
+  OneShotLayerTable,
+  TapDanceTable,
+  LeaderTable,
+  StenoTable,
+  SpaceCadetTable,
 
-    BlankTable
-]
+  BlankTable
+];
+
+/*const language = getLocalStorage("language") || "eng";*/
+baseKeyCodeTable = withLanguageLayout(baseKeyCodeTable, spanish);
+
 const keyCodeTable = baseKeyCodeTable
-    .concat(ModifiedLetterTables)
-    .concat(ModifiedDigitTables)
-    .concat(ModifiedPunctuationTables)
-    .concat(ModifiedSpacingTables)
-    .concat(ModifiedNavigationTables)
-    .concat(ModifiedModifiersTables)
-    .concat(HyperMehTable)
-    .concat(ModifiedFunctionKeyTables)
-    .concat(ModifiedNumpadTables)
-    .concat(DualUseModifierTables)
-    .concat(DualUseLayerTables)
+  .concat(ModifiedLetterTables)
+  .concat(ModifiedDigitTables)
+  .concat(ModifiedPunctuationTables)
+  .concat(ModifiedSpacingTables)
+  .concat(ModifiedNavigationTables)
+  .concat(ModifiedModifiersTables)
+  .concat(HyperMehTable)
+  .concat(ModifiedFunctionKeyTables)
+  .concat(ModifiedNumpadTables)
+  .concat(DualUseModifierTables)
+  .concat(DualUseLayerTables);
 
 class KeymapDB {
-    constructor() {
-        this.keymapCodeTable = []
+  constructor() {
+    this.keymapCodeTable = [];
 
-        for (let group of keyCodeTable) {
-            for (let key of group.keys) {
-                let value
+    for (let group of keyCodeTable) {
+      for (let key of group.keys) {
+        let value;
 
-                if (key.labels) {
-                    value = key
-                } else {
-                    value = {
-                        code: key.code,
-                        labels: {
-                            primary: "#" + key.code.toString()
-                        }
-                    }
-                }
-
-                this.keymapCodeTable[key.code] = value
+        if (key.labels) {
+          value = key;
+        } else {
+          value = {
+            code: key.code,
+            labels: {
+              primary: "#" + key.code.toString()
             }
+          };
         }
+
+        this.keymapCodeTable[key.code] = value;
+      }
+    }
+  }
+
+  parse(keyCode) {
+    let key;
+
+    if (!keyCode) keyCode = 0;
+
+    if (keyCode < this.keymapCodeTable.length) {
+      key = this.keymapCodeTable[keyCode];
     }
 
-    parse(keyCode) {
-        let key
-
-        if (!keyCode) keyCode = 0
-
-        if (keyCode < this.keymapCodeTable.length) {
-            key = this.keymapCodeTable[keyCode]
+    if (!key) {
+      key = {
+        code: keyCode,
+        labels: {
+          primary: "#" + keyCode.toString()
         }
-
-        if (!key) {
-            key = {
-                code: keyCode,
-                labels: {
-                    primary: "#" + keyCode.toString()
-                }
-            }
-        }
-
-        return {
-            keyCode: key.code,
-            label: key.labels.primary,
-            extraLabel: key.labels.top,
-            verbose: key.labels.verbose
-        }
+      };
     }
 
-    serialize(key) {
-        return key.keyCode
-    }
+    return {
+      keyCode: key.code,
+      label: key.labels.primary,
+      extraLabel: key.labels.top,
+      verbose: key.labels.verbose
+    };
+  }
+
+  serialize(key) {
+    return key.keyCode;
+  }
 }
 
-export { KeymapDB as default, baseKeyCodeTable, keyCodeTable }
+export { KeymapDB as default, baseKeyCodeTable, keyCodeTable };
