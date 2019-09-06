fbd93af139f784c34564dda1586924be5b1bc431
anton-voron <anton.voron.gt@gmail.com>
2019-09-06 18:26:37 +0300

    Create spanish layout

---
+++ packages/chrysalis-keymap/src/lib/languages/spanish/spanish.js
@@ -0,0 +1,220 @@
+const spanish = [
+  {
+    code: 45,
+    labels: {
+      primary: "'"
+    }
+  },
+  {
+    code: 46,
+    labels: {
+      primary: "¡"
+    }
+  },
+  {
+    code: 47,
+    labels: {
+      primary: "`"
+    }
+  },
+  {
+    code: 48,
+    labels: {
+      primary: "+"
+    }
+  },
+  {
+    code: 49,
+    labels: {
+      primary: "ç"
+    }
+  },
+  {
+    code: 51,
+    labels: {
+      primary: "ñ"
+    }
+  },
+  {
+    code: 52,
+    labels: {
+      primary: "´"
+    }
+  },
+  {
+    code: 56,
+    labels: {
+      primary: "-"
+    }
+  },
+  {
+    code: 2079,
+    labels: {
+      primary: '"'
+    }
+  },
+  {
+    code: 2080,
+    labels: {
+      primary: "·"
+    }
+  },
+  {
+    code: 2083,
+    labels: {
+      primary: "&"
+    }
+  },
+  {
+    code: 2084,
+    labels: {
+      primary: "/"
+    }
+  },
+  {
+    code: 2085,
+    labels: {
+      primary: "("
+    }
+  },
+  {
+    code: 2086,
+    labels: {
+      primary: ")"
+    }
+  },
+  {
+    code: 2087,
+    labels: {
+      primary: "="
+    }
+  },
+  {
+    code: 2093,
+    labels: {
+      primary: "?"
+    }
+  },
+  {
+    code: 2094,
+    labels: {
+      primary: "¿"
+    }
+  },
+  {
+    code: 2095,
+    labels: {
+      primary: "^"
+    }
+  },
+  {
+    code: 2096,
+    labels: {
+      primary: "*"
+    }
+  },
+  {
+    code: 2097,
+    labels: {
+      primary: "Ç"
+    }
+  },
+  {
+    code: 2099,
+    labels: {
+      primary: "Ñ"
+    }
+  },
+  {
+    code: 2100,
+    labels: {
+      primary: "¨"
+    }
+  },
+  {
+    code: 2102,
+    labels: {
+      primary: ";"
+    }
+  },
+  {
+    code: 2103,
+    labels: {
+      primary: ":"
+    }
+  },
+  {
+    code: 2104,
+    labels: {
+      primary: "_"
+    }
+  },
+  {
+    code: 798,
+    labels: {
+      primary: "|"
+    }
+  },
+  {
+    code: 799,
+    labels: {
+      primary: "@"
+    }
+  },
+  {
+    code: 800,
+    labels: {
+      primary: "#"
+    }
+  },
+  {
+    code: 801,
+    labels: {
+      primary: "~"
+    }
+  },
+  {
+    code: 802,
+    labels: {
+      primary: "€"
+    }
+  },
+  {
+    code: 803,
+    labels: {
+      primary: "¬"
+    }
+  },
+  {
+    code: 776,
+    labels: {
+      primary: "€"
+    }
+  },
+  {
+    code: 815,
+    labels: {
+      primary: "["
+    }
+  },
+  {
+    code: 816,
+    labels: {
+      primary: "]"
+    }
+  },
+  {
+    code: 817,
+    labels: {
+      primary: "}"
+    }
+  },
+  {
+    code: 820,
+    labels: {
+      primary: "{"
+    }
+  }
+];
+
+export default spanish;
